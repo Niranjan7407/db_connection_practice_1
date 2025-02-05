8397ee62a7525c069d825a2eee8f18df4243cf0c
Niranjan R <palanisamyniranjan69@gmail.com>
2025-02-05 12:47:05 +0530

    Completed the assignment

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const { resolve } = require('path');
+require('dotenv').config();
+const connectDB=require('./database/db')
 
 const app = express();
 const port = 3010;
+const db_url=process.env.db_url
 
 app.use(express.static('static'));
 
@@ -12,4 +15,6 @@ app.get('/', (req, res) => {
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
+  connectDB(db_url)
 });
+
